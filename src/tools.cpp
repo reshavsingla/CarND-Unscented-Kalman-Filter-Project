c3b5f295f615a2180266ff7c78d702b5db8bec19
reshav <singla.reshav@gmail.com>
2018-03-15 19:52:21 -0700

    Solution

---
+++ src/tools.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+  TODO:
+    * Calculate the RMSE here.
+  */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+
+    if(estimations.size() != ground_truth.size() || estimations.empty()){
+        cout << "Invalid estimation or ground_truth data" << endl;
+        return rmse;
+    }
+
+    for(int i = 0;i < estimations.size();++i)
+    {
+        VectorXd diff = estimations[i] - ground_truth[i];
+        diff = diff.array()*diff.array();
+        rmse += diff;
+    }
+
+    rmse = rmse/estimations.size();
+    rmse = rmse.array().sqrt();
+    return rmse;
+}
